4bd10680c33fb7bb8ac3e11ec40e5bdb3688dfc3
luiscastror <ing.lcr.21@gmail.com>
2023-02-01 17:05:02 -0500

    Ajustados

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { MainService } from '../services/main.service';
 
 @Component({
@@ -10,10 +11,19 @@ export class Tab1Page implements OnInit {
   items: any[] = [];
   collection: string = 'items';
 
-  constructor(public MainService: MainService) {}
+  constructor(
+    public MainService: MainService,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit(): void {
     this.get();
+  }
+
+  results: any;
+  handleChange(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.items.filter(d => d.name.toLowerCase().indexOf(query) > -1);
   }
 
   get() {
@@ -23,10 +33,13 @@ export class Tab1Page implements OnInit {
         this.items.push({ ...doc.data(), uid: doc.id });
       });
       console.log(this.items);
+
+      this.results = [...this.items];
     });
   }
 
   addCart(item: any) {
+    this.presentToast('top');
     const pos = this.MainService.cart.findIndex((x: any) => x.uid == item.uid);
     if (pos == -1) {
       this.MainService.cart.push({
@@ -37,4 +50,15 @@ export class Tab1Page implements OnInit {
       this.MainService.cart[pos].count++;
     }
   }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Item added correctly',
+      duration: 1500,
+      position: position
+    });
+
+    await toast.present();
+  }
+
 }
